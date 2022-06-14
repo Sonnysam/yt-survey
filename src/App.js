978259a494cef6f8cf6a132611c1d462ec77e19c
Samuel Agbenyo <80902426+Sonnysam@users.noreply.github.com>
2022-06-14 14:29:47 +0000

    image added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import './App.css';
-import { Button } from "react-bootstrap";
+import { Form } from "react-bootstrap";
+import logo from './logo.png';
 
 function App() {
   return (
-    <>
-      <div>hello world</div>
-      <Button variant="outline-primary">Primary</Button>{" "}
-    </>
+    <div className='container p-5'>
+      <div className="logo d-flex align-items-center justify-content-center">
+        <img 
+          src={logo} 
+          alt="logo" 
+          className="logo-img"
+          style={{width: '200px', height: '200px'}}
+        />
+      </div>
+      <Form>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="name@example.com" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+          <Form.Label>Example textarea</Form.Label>
+          <Form.Control as="textarea" rows={3} />
+        </Form.Group>
+      </Form>
+    </div>
   );
 }
 
